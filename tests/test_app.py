302c5f9ebbf6c63deb812b8a6c7aca2fa50c5e6d
Ashley Parks <aqparks@gmail.com>
2021-11-02 23:07:14 -0700

    initial configuration

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,9 @@
-from .context import blueprint
+from .context import application_gateway
 
 
 def test_app(capsys, example_fixture):
     # pylint: disable=W0612,W0613
-    blueprint.Blueprint.run()
+    application_gateway.Blueprint.run()
     captured = capsys.readouterr()
 
     assert "Hello World..." in captured.out
